dff1116976fd9ee9cfb3f0b7f983d1b887e6225b
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-12-11 14:52:16 -0300

    Primeiras inserções na página principal

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,2 +1,6 @@
+if(!sessionStorage.getItem('username')){
+    window.location.href = './pages/login.html'
+}
+
 const usernameTitle = document.getElementById('username-title');
 usernameTitle.innerText = sessionStorage.getItem('username')
