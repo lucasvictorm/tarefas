3b71939c4278989e4e1a4af298f3291be8ffcd31
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-12-11 16:48:55 -0300

    preparação do backend para receber tasks

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const loginRouter = require('./loginRouter.js');
+const tasksRouter = require('./tasksRouter.js')
 
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
+router.use('/tasks', tasksRouter);
 
 module.exports = router;
