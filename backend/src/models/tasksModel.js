f8435e993ae46454c607b46881eb68c8cd360d9b
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-12-11 22:32:58 -0300

    correção na api de recebimento de tasks

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,9 +2,8 @@ const mysql = require('./connection.js');
 
 class TasksModel{
     async getTasks(id){
-        const [tasksList] = await mysql.execute('SELECT task_id, task_name, task_date, task_status from tasks where task_user=?',[id]);
-        console.log(tasksList)
-        const tasks = tasksList[0];
+        const [tasks] = await mysql.execute('SELECT task_id, task_name, task_date, task_status from tasks where task_user=?',[id]);
+        
         return tasks;
     }
 }
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,10 +6,25 @@ const usernameTitle = document.getElementById('welcome-name');
 usernameTitle.innerText = sessionStorage.getItem('username')
 
 const body = document.querySelector('body');
-body.addEventListener('load', loadtasks);
+//body.addEventListener('load', loadtasks);
 
-function fetchGetTasks(id){
-    const tasks = {} 
-    fetch(`http://localhost:3000/tasks${id}`)
-    .then()
+async function fetchGetTasks(id){
+    let tasks = {} 
+    await fetch(`http://localhost:3000/tasks/${id}`)
+    .then(async (res) => {
+        return await res.json()
+    })
+    .then((data) => {
+        tasks = data
+    })
+    .catch(err => console.log(err))
+    console.log(tasks)
+    return tasks;
 }
+
+async function loadTasks(){
+    const id = sessionStorage.getItem('id');
+    await fetchGetTasks(id)
+}
+
+loadTasks()
